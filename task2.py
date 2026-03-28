04fdfe89dbb57f71f925cc0398453f4b5e5cc3c3
yana-shapka <57398010+yana-shapka@users.noreply.github.com>
2026-03-28 20:47:33 +0200

    Initial commit

---
+++ task2.py
@@ -0,0 +1,16 @@
+import re
+from typing import Callable
+
+
+def generator_numbers(text: str):
+    for match in re.finditer(r'\b\d+\.\d+\b', text):
+        yield float(match.group())
+
+
+def sum_profit(text: str, func: Callable):
+    return sum(func(text))
+
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
